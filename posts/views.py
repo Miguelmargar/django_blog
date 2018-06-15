e89b759551faf88dc978eff5bee99480e3bf0f3b
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-15 14:36:10 +0000

    fixed images issues in both blogposts.html and postdetail.html

---
+++ posts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+# Create your views here.
+def get_posts(request):
+    posts = Post.objects.all()
+    return render(request, "posts/blogposts.html", {'posts': posts})
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.views += 1
+    post.save()
+    return render(request, "posts/postdetail.html", {'post':post})
+    
+def create_or_edit_post(request, pk):
+    
