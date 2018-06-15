16a6291cabc4fe2aae3029d56a2a3857afc99d8f
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-15 15:09:50 +0000

    added edit post page and functionality

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import BlogPostForm
 
 # Create your views here.
 def get_posts(request):
@@ -12,5 +13,19 @@ def post_detail(request, pk):
     post.save()
     return render(request, "posts/postdetail.html", {'post':post})
     
-def create_or_edit_post(request, pk):
+def create_or_edit_post(request, pk=None):
+    """
+    Create a view that allows us to create
+    or edit a post depending if the Post ID
+    is null or not
+    """
+    post = get_object_or_404(Post, pk=pk) if pk else None
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(post_detail, post.pk)
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'posts/blogpostform.html', {'form': form})
     
