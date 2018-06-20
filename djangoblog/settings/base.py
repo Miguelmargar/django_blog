3c4180271ea33546785c7020521993296c8cadd0
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-20 15:25:56 +0000

    added allowed hosts for heroku

--- djangoblog/settings/base.py
+++ djangoblog/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-miguelmargar.c9users.io']
+ALLOWED_HOSTS = ['django-blog-miguelmargar.c9users.io', 'migdjangoblog.herokuapp.com']
 
 
 # Application definition
